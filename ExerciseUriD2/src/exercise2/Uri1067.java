72857583c6b8a407b06a646abab06f12c2dd597b
João Vítor Souza Pioner <joaopioner@outlook.com>
2020-07-09 19:37:40 -0300

    Adicionei mais exercícios.

---
+++ ExerciseUriD2/src/exercise2/Uri1067.java
@@ -0,0 +1,17 @@
+package exercise2;
+
+import java.util.Scanner;
+
+public class Uri1067 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		int x = in.nextInt();
+		for (int i = 1; i < x || i == x == (x % 2 != 0); i += 2) {
+			System.out.println(i);
+			}
+		in.close();
+	}
+
+}
