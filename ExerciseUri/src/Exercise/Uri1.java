445e2b144c85a8d61bed55c4189ef97147bc0f92
João Vítor Souza Pioner <66327190+joaopioner1@users.noreply.github.com>
2020-07-09 19:41:57 -0300

    Retirei espaços inúteis.

--- ExerciseUri/src/Exercise/Uri1.java
+++ ExerciseUri/src/Exercise/Uri1.java
@@ -3,18 +3,15 @@ import  java.util.Scanner;
 public class Uri1 {
 
 	public Uri1() {
-		
-	}
-
+		}
 	public static void main(String[] args) {
-Scanner in = new Scanner(System.in);
+	Scanner in = new Scanner(System.in);
 		
 		int a, b;
 		
 		a = in.nextInt();
 		b = in.nextInt();
-	
-		
+
 		int soma = a + b;
 		System.out.println("X = " + soma);
 
