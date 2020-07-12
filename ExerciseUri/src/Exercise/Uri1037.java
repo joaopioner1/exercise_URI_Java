c523d47b7fa56c59924c71aefed72530d2cd37a0
João Vítor Souza Pioner <66327190+joaopioner1@users.noreply.github.com>
2020-07-11 21:25:28 -0300

    Fiz pequenas mudanças.

--- ExerciseUri/src/Exercise/Uri1037.java
+++ ExerciseUri/src/Exercise/Uri1037.java
@@ -13,10 +13,9 @@ public class Uri1037 {
 		double numeroInput = in.nextDouble();
 
 		if (numeroInput < 0 || numeroInput > 100) {
-			System.out.println("Fora de intervalo");
-		}
-
-		else if (numeroInput >= 0 && numeroInput <= 25.0000) {
+			System.out.print("Fora de intervalo");
+		
+		}else if (numeroInput >= 0 && numeroInput <= 25.0000) {
 			System.out.printf("Intervalo [0,25]");
 		} else if (numeroInput > 25 && numeroInput <= 50) {
 			System.out.printf("Intervalo [25,50]");
