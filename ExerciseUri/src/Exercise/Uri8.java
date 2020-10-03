42e6c9292e00f3828ddf93083eec0a124aea18d1
Joao Vitor Pioner <joaopioner123@gmail.com>
2020-10-02 22:13:09 -0300

    Update Uri8.java

--- ExerciseUri/src/Exercise/Uri8.java
+++ ExerciseUri/src/Exercise/Uri8.java
@@ -9,12 +9,12 @@ public class Uri8 {
 
 	public static void main(String[] args) {
 		
-		//Agora, em vez de usar vírgulas, o programa usa pontos.
+		//Agora, em vez de usar vÃ­rgulas, o programa usa pontos.
 		Locale.setDefault(Locale.US);
 		Scanner entrada = new Scanner(System.in);
 		 
 		int numeroFuncionarios;
-		float horasTrabalhadas, valorPorHora, salario;
+		double horasTrabalhadas, valorPorHora, salario;
 		
 		numeroFuncionarios = entrada.nextInt(); 
 		horasTrabalhadas = entrada.nextFloat();
